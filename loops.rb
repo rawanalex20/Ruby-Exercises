5972bde648d43bce80b801ee1f732d0b4c260365
rawan <rawanalex15@gmail.com>
2022-01-15 22:57:09 +0200

    incomplete - intial loops.rb commit

---
+++ loops.rb
@@ -0,0 +1,100 @@
+# A. Print out the array elements
+def progA_for
+end
+
+
+def progA_while
+end
+
+
+def progA_loop
+end
+
+
+# B. Print out the array elements except 5 and 6
+def progB_for
+end
+
+
+def progB_while
+end
+
+
+def progB_loop
+end
+
+
+# C. Print out array elements less than 6
+def progC_for
+end
+
+
+def progC_while
+end
+
+
+def progC_loop
+end
+
+
+#D. Print out even numbers only
+def progD_for
+end
+
+
+def progD_while
+end
+
+
+def progD_loop
+end
+
+
+# E. Print out odd numbers only
+def progE_for
+end
+
+
+def progE_while
+end
+
+
+def progE_loop
+end
+
+
+# F. Print out numbers from 2 to 9
+def progF_for
+end
+
+
+def progF_while
+end
+
+
+def progF_loop
+end
+
+
+=begin G. Print out each number indicating whether it's even or odd
+Ex: 1 odd
+2 even
+3 odd
+4 even .. 
+=end
+def progG_for
+end
+
+
+def progG_while
+end
+
+
+def progG_loop
+end
+
+
+arr = [1,2,3,4,5,6,7,8,9,10]
+
+### call method here
+
