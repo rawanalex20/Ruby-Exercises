cb3fb6436984ce55c85ca9abc1335bc0213e8604
rawan <rawanalex15@gmail.com>
2022-01-15 17:24:28 +0200

    update2 numbers.rb

--- numbers.rb
+++ numbers.rb
@@ -1,38 +1,49 @@
 x = 1
+
+# A. Print out the binary presentation of 1
 puts x.to_s(2)
 
+# B. Print out whether it’s even or odd
 if x.even?
     puts "even"
 else
     puts "odd"
 end
 
-x = x.to_f
-
+# C. Subtract 2, print the result
 x -= 2
 puts x
 
+# D. Divide the result by 2,print the result
+x = x.to_f
 x /= 2
 puts x
 
+# E. Add 1/1/4, print the result
 x += 5 / 4
 puts x
 
-puts "Numerator " + x.numerator.to_s
-puts "Denomerator " + x.denominator.to_s
+# F. print the numerator and denominator of result
+puts "Numerator: #{x.numerator.to_s}"
+puts "Denomerator: #{x.denominator.to_s}"
 
+# G. Multiply by ⅓ , print the result
 x = x * 1 / 3
 puts x
 
+# H. Round to 3 decimal places, print the result
 x = x.round(3)
 puts x
 
+# I. Save the result as a string y, print the result
 y = x.to_s
 puts y
 
+# J. Convert y to integer,print it
 y = y.to_i
 puts y
 
+# K. Convert y to decimal,print it
 y = y.to_f
 puts y
 
